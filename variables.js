1b7b7b7b6bac1fc60466eccd57c872aa05e90a98
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-01-10 16:51:18 -0500

    Add semicolon to the end of each variable line

--- variables.js
+++ variables.js
@@ -1,11 +1,11 @@
-var favoriteCar = "toyota camry"
-var hatType = "cowboy"
-var weatherStatus = "rainy"
+var favoriteCar = "toyota camry";
+var hatType = "cowboy";
+var weatherStatus = "rainy";
 
-var gasPrice = "3.89"
-var speedLimit = "65"
-var numberOfCats = "1"
+var gasPrice = "3.89";
+var speedLimit = "65";
+var numberOfCats = "1";
 
-var lightsOn = "true"
-var tankFull = "false"
-var dinnerTime = "false"
+var lightsOn = "true";
+var tankFull = "false";
+var dinnerTime = "false";
