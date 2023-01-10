b266ed6e943ed2a059b37b7d444083c73a588879
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-01-10 17:03:56 -0500

    Add multiple lines of console.log

--- variables.js
+++ variables.js
@@ -1,6 +1,6 @@
-var favoriteCar = "toyota camry";
-var hatType = "cowboy";
-var weatherStatus = "rainy";
+var favoriteCar = "toyota camry"//jeep cheroke;
+var hatType = "cowboy"//baseball;
+var weatherStatus = "rainy"//sunny;
 
 var gasPrice = "3.89";
 var speedLimit = "65";
@@ -9,3 +9,11 @@ var numberOfCats = "1";
 var lightsOn = "true";
 var tankFull = "false";
 var dinnerTime = "false";
+ 
+console.log("hello there!")
+console.log("Not sure semicolons were the right choice")
+console.log("I suppose we shall see")
+console.log("Hope I am understanding JavaScript so far")
+console.log("Can't wait to run this code!")
+console.log("One more should do it.")
+console.log("We are done with logs!")
